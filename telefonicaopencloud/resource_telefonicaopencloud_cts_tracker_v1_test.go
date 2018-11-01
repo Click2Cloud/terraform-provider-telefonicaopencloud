ae9b692a4c640b90a242fe24a2dbb0293a4fc736
d-apurva <apurva.dhoble@click2cloud.net>
2018-11-01 17:44:34 +0530

    modify acc test

--- telefonicaopencloud/resource_telefonicaopencloud_cts_tracker_v1_test.go
+++ telefonicaopencloud/resource_telefonicaopencloud_cts_tracker_v1_test.go
@@ -131,11 +131,6 @@ resource "telefonicaopencloud_smn_topic_v2" "topic_1" {
 resource "telefonicaopencloud_cts_tracker_v1" "tracker_v1" {
   bucket_name      = "${telefonicaopencloud_s3_bucket.bucket.bucket}"
   file_prefix_name      = "yO8Q"
-  is_support_smn = true
-  topic_id = "${telefonicaopencloud_smn_topic_v2.topic_1.id}"
-  is_send_all_key_operation = false
-  operations = ["login"]
-  need_notify_user_list = ["user1"]
 }
 `
 
@@ -152,11 +147,6 @@ resource "telefonicaopencloud_smn_topic_v2" "topic_1" {
 resource "telefonicaopencloud_cts_tracker_v1" "tracker_v1" {
   bucket_name      = "${telefonicaopencloud_s3_bucket.bucket.bucket}"
   file_prefix_name      = "yO8Q1"
-  is_support_smn = true
-  topic_id = "${telefonicaopencloud_smn_topic_v2.topic_1.id}"
-  is_send_all_key_operation = false
-  operations = ["login"]
-  need_notify_user_list = ["user1"]
 }
 `
 
@@ -175,11 +165,6 @@ resource "telefonicaopencloud_smn_topic_v2" "topic_1" {
 resource "telefonicaopencloud_cts_tracker_v1" "tracker_v1" {
   bucket_name      = "${telefonicaopencloud_s3_bucket.bucket.bucket}"
   file_prefix_name      = "yO8Q"
-  is_support_smn = true
-  topic_id = "${telefonicaopencloud_smn_topic_v2.topic_1.id}"
-  is_send_all_key_operation = false
-  operations = ["login"]
-  need_notify_user_list = ["user1"]
 
 timeouts {
     create = "5m"
